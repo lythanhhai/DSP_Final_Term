dfef3b852dd7a4a8668d0846ee90df3104ae8c4f
lythanhhai <you@example.com>
2021-11-27 23:02:24 +0700

    add magnitude stardard

--- figureExamV_UV.m
+++ figureExamV_UV.m
@@ -1,8 +1,11 @@
-Fo_studio_fe = V_UV('./fileTinHieuMoi/studio_F1.wav', 'studio female');
+%Fo_studio_fe = V_UV('./fileTinHieuMoi/studio_F1.wav', 'studio female');
 %Fo_studio_ma = V_UV('./fileTinHieuMoi/studio_M1.wav', 'studio male');
 %Fo_phone_fe = V_UV('./fileTinHieuMoi/phone_F1.wav', 'phone female');
 %Fo_phone_ma = V_UV('./fileTinHieuMoi/phone_M1.wav', 'phone male');
-%Fo_studio_fe1 = V_UV('./tinHieuHuanLuyen/01MDA.wav', 'studio female');
+
+MDA01 = [0.45, 0.81, 1.53, 1.85, 2.69, 2.86, 3.78, 4.15, 4.84, 5.14];
+
+Fo_studio_fe1 = V_UV('./tinHieuHuanLuyen/01MDA.wav', 'studio female', MDA01);
 %Fo_studio_ma = V_UV('./tinHieuHuanLuyen/02FVA.wav', 'studio male');
 %Fo_phone_fe = V_UV('./tinHieuHuanLuyen/03MAB.wav', 'phone female');
 %Fo_phone_ma = V_UV('./tinHieuHuanLuyen/06FTB.wav', 'phone male');
